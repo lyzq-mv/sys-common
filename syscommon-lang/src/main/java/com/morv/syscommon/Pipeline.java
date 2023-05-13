a4e77d8a2bc93b34e3859092fd8f04cb5ef53128
luoyezq <chalmer_cm@163.com>
2023-05-13 14:57:25 +0800

    add code

--- syscommon-lang/src/main/java/com/morv/syscommon/Pipeline.java
+++ syscommon-lang/src/main/java/com/morv/syscommon/Pipeline.java
@@ -1,38 +1,17 @@
 package com.morv.syscommon;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-/**
- * 管道，用于处理流式计算逻辑
- *
- * @param <T> 管道中元数类型
- */
-public interface Pipeline<T> {
-    void each(Consumer<T> consumer);
 
-    /**
-     * 通过集合获取管道
-     *
-     * @param collection
-     * @param <T>
-     * @return
-     */
+public interface Pipeline<T> {
     static <T> Pipeline<T> of(Collection<T> collection) {
         return collection::forEach;
     }
 
-    /**
-     * 通过数据获取管道
-     *
-     * @param args
-     * @param <T>
-     * @return
-     */
     static <T> Pipeline<T> of(T... args) {
         return c -> {
             for (T arg : args) {
@@ -41,23 +20,29 @@ public interface Pipeline<T> {
         };
     }
 
-    /**
-     * map方法,加工出新的元素
-     *
-     * @param function
-     * @param <R>
-     * @return
-     */
+    void each(Consumer<T> consumer);
+
     default <R> Pipeline<R> map(Function<T, R> function) {
         return c -> each(t -> c.accept(function.apply(t)));
     }
 
-    /**
-     * 过滤方法
-     *
-     * @param predicate 过来条件
-     * @return
-     */
+    default <R> Pipeline<R> flatMap(Function<T, Pipeline<R>> function) {
+        return c -> each(t -> {
+            Pipeline<R> apply = function.apply(t);
+            apply.each(c);
+        });
+    }
+
+    default <R> Pipeline<R> flatMapByColl(Function<T, Collection<R>> function) {
+        return c -> each(t -> {
+            Collection<R> apply = function.apply(t);
+            for (R r : apply) {
+                c.accept(r);
+            }
+        });
+    }
+
+
     default Pipeline<T> filter(Predicate<T> predicate) {
         return c -> each(t -> {
             if (predicate.test(t)) {
@@ -73,15 +58,41 @@ public interface Pipeline<T> {
         });
     }
 
-    /**
-     * 结束方法，将处理后的元素放入List
-     *
-     * @return
-     */
     default List<T> toList() {
         List<T> list = new ArrayList<>();
         each(list::add);
         return list;
+    }
+
+    default <K, V> Map<K, V> toMap(Function<T, K> keyFunction, Function<T, V> valueFunction) {
+        Map<K, V> map = new HashMap<>();
+        each(t -> {
+            K key = keyFunction.apply(t);
+            if (map.containsKey(key)) {
+                throw new IllegalArgumentException("duplicate key:" + key);
+            }
+            map.put(key, valueFunction.apply(t));
+        });
+        return map;
+    }
+
+    default Optional<T> findFirst(Predicate<T> predicate) {
+        AtomicReference<T> reference = new AtomicReference<>();
+        try {
+            each(t -> {
+                if (predicate.test(t)) {
+                    reference.set(t);
+                    throw new StopException();
+                }
+            });
+        } catch (StopException e) {
+            //
+        }
+        return Optional.ofNullable(reference.get());
+    }
+
+    static class StopException extends RuntimeException {
+
     }
 
 }
---
+++ syscommon-lang/src/test/java/com/morv/syscommon/lang/test/PipelineTest.java
@@ -0,0 +1,29 @@
+package com.morv.syscommon.lang.test;
+
+import com.morv.syscommon.Pipeline;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.testng.Assert.assertEquals;
+
+public class PipelineTest {
+    @Test
+    public void basicTest() {
+        List<AtomicInteger> list = Pipeline
+                .of(new AtomicInteger(1)
+                        , new AtomicInteger(2)
+                        , new AtomicInteger(3)
+                        , new AtomicInteger(4)
+                        , new AtomicInteger(5))
+                .filter(it -> it.get() > 2)
+                .map(it -> new AtomicInteger(it.get() + 1))
+                .peek(it -> it.getAndAdd(3))
+                .toList();
+        assertEquals(list.size(), 3);
+        assertEquals(list.get(0).get(), 7);
+        assertEquals(list.get(1).get(), 8);
+        assertEquals(list.get(2).get(), 9);
+    }
+}
